7d87762a251685d77cee4cef00f20d244d75a089
twkimfe <arthurkim135@naver.com>
2025-01-01 15:57:20 +0900

    feat: Todo 수정 기능 리팩토링 - TodoService로 EditMode 상태 관리 이관 - 이벤트 핸들러 바인딩 및 정리 로직 개선 - 폼 데이터 처리 및 에러 핸들링 강화 - 메모리 누수 방지를 위한 이벤트 리스너 정리
    기능 에러 수정 시도했으니 실패, 추가 디버깅 필요

--- src/js/pages/TodoList.js
+++ src/js/pages/TodoList.js
@@ -225,22 +225,19 @@ class TodoList {
   async editTodo(todoId) {
     console.log("받은 todoId:", todoId, typeof todoId);
     console.log("현재 todos:", this.todos);
-    const todo = this.todos.find((todo) => todo.id === +todoId);
+    const todo = this.todos.find((todo) => todo.id === Number(todoId));
     console.log("찾은 todo:", todo);
     console.log(
       "현재 todos 상세:",
       this.todos.map((todo) => ({ id: todo.id, type: typeof todo.id }))
     );
     if (todo) {
+      const editEvent = new CustomEvent("editTodoRequested", {
+        detail: { todo },
+        bubbles: true,
+      });
       // todoForm을 호출하고 기존 데이터를 전달하는 커스텀 이벤트 발생
-      document.dispatchEvent(
-        new CustomEvent("editTodoRequested", {
-          detail: {
-            todo: todo,
-          },
-          bubbles: true,
-        })
-      );
+      document.dispatchEvent(editEvent);
       console.log("editTodoRequested 이벤트 발생 완료"); // 이벤트 발생 확인
       if (window.router) {
         window.router.navigate("/edit");
